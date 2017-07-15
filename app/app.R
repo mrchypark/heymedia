7868abc88cf5b0e041e0f3832fdc45f6d4c62e50
mrchypark <mrchypark@gmail.com>
2017-07-16 00:28:44 +0900

    다시

--- app/app.R
+++ app/app.R
@@ -6,8 +6,8 @@ flask = import('flask')
 app = flask$Flask('__main__')
 
 index = function() {
-  render("/app/static/index.Rmd")
-  return("/app/static/index.html")
+  ret <- render("/app/static/index.Rmd")
+  return(ret)
   }
 app$add_url_rule('/', 
                  methods=list("GET","POST"),
