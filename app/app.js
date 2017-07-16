2b41179ab5ca71bfe59728cd3e653232c2044258
mrchypark <mrchypark@gmail.com>
2017-07-16 22:01:04 +0900

    sendFile

--- app/app.js
+++ app/app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/views'));
+
 app.get('/', function(req, res) {
-    res.render('index.html');
+    res.sendFile('index.html');
 });
 
 app.listen(3000, function() {
