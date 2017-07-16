da386561288f7ffb636b250935681bc3627ea011
mrchypark <mrchypark@gmail.com>
2017-07-16 21:21:56 +0900

    html

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('index.html');
 });
 
 app.listen(3000, function() {
