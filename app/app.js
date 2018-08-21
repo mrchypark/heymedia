71bdeabb745b27e9ec21b71f226b899a5ec44a48
Chan-Yub Park <mrchypark@gmail.com>
2018-08-22 01:50:47 +0900

    Update app.js

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ app.get('/', function(req, res) {
     res.sendFile('index.html');
 });
 
-app.listen(3000, function() {
+app.listen(3000, "0.0.0.0",function() {
     
     console.log('Example app listening on port 3000!');
 });
